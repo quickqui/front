c9366074639b57c7a2377cd7a37d938dc6c7a459
nielinjie <nielinjie@gmail.com>
2019-08-15 10:59:31 +0800

    环境开关

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,43 +1,50 @@
 const proxy = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//     app.use(proxy('/model-server', {
-//         target: 'http://model-server:1111', pathRewrite: {
-//             '^/model-server/dataModel': '/dataModel',
-//             '^/model-server/model':'/model',
-//             '^/model-server/deploy': '/deploy'
-//         }
-//     }));
-//     app.use(proxy('/prisma', {
-//         target: 'http://prisma:4466', pathRewrite: {
-//             '^/prisma': '/'
-//         }
-//     }))
-//     app.use(proxy('/app', {
-//         target: 'http://app-server:4000', pathRewrite: {
-//             '^/app': '/'
-//         }
-//     }))
-// };
 
 
+const env = process.env.ENV
 
 module.exports = function (app) {
-    app.use(proxy('/model-server', {
-        target: 'http://localhost:1111', pathRewrite: {
-            '^/model-server/dataModel': '/dataModel',
-            '^/model-server/model':'/model',
-            '^/model-server/deploy': '/deploy'
-        }
-    }));
-    app.use(proxy('/prisma', {
-        target: 'http://localhost:4466', pathRewrite: {
-            '^/prisma': '/'
-        }
-    }))
-    app.use(proxy('/app', {
-        target: 'http://localhost:4000', pathRewrite: {
-            '^/app': '/'
-        }
-    }))
+    console.log(env)
+    if (env == 'dev_docker') {
+
+        app.use(proxy('/model-server', {
+            target: 'http://model-server:1111', pathRewrite: {
+                '^/model-server/dataModel': '/dataModel',
+                '^/model-server/model': '/model',
+                '^/model-server/deploy': '/deploy'
+            }
+        }));
+        app.use(proxy('/prisma', {
+            target: 'http://prisma:4466', pathRewrite: {
+                '^/prisma': '/'
+            }
+        }))
+        app.use(proxy('/app', {
+            target: 'http://app-server:4000', pathRewrite: {
+                '^/app': '/'
+            }
+        }))
+    }
+    if (env == 'dev_local') {
+        app.use(proxy('/model-server', {
+            target: 'http://localhost:1111', pathRewrite: {
+                '^/model-server/dataModel': '/dataModel',
+                '^/model-server/model': '/model',
+                '^/model-server/deploy': '/deploy'
+            }
+        }));
+        app.use(proxy('/prisma', {
+            target: 'http://localhost:4466', pathRewrite: {
+                '^/prisma': '/'
+            }
+        }))
+        app.use(proxy('/app', {
+            target: 'http://localhost:4000', pathRewrite: {
+                '^/app': '/'
+            }
+        }))
+    }
 };
+
+
